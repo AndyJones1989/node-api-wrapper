9d1f589148de7971a0bc815391b45337d9af9c04
Andrew <andrew.jones@and.digital>
2024-02-06 13:29:59 +0000

    before deployment

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
 const express = require("express");
 const OpenAI = require("openai");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+const path = require("path");
 require("dotenv").config();
 
 const app = express();
 
-app.get("/", async (req, res) => {
-  const data = await getAIResponse("Name 3 types of apples");
+app.use(bodyParser.json());
+
+app.post("/", async (req, res) => {
+  if (!checkRateLimit(req)) {
+    return res.status(429).json({ message: "Rate limit exceeded" });
+  }
+  writeToRateLimiter(req);
+  const reqMessage = req.body.question;
+  const data = await getAIResponse(reqMessage);
   return res.json({ message: data.content });
 });
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+  console.log("Server is running on 3000");
 });
-
-// exports.app = functions.https.onRequest(app);
 
 async function getAIResponse(message) {
   const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
   });
+  if (!message) {
+    return { content: "Please provide a message to get a response" };
+  }
   const response = await openai.chat.completions.create({
     model: "gpt-3.5-turbo",
     messages: [
@@ -35,3 +46,45 @@ async function getAIResponse(message) {
   });
   return response.choices[0].message;
 }
+
+function writeToRateLimiter(req) {
+  const requester = req.headers["user-agent"];
+  const timestamp = new Date().toISOString();
+
+  const rateLimitPath = path.join(__dirname, "rateLimit.json");
+  let rateLimitData = {};
+  if (fs.existsSync(rateLimitPath)) {
+    const fileContent = fs.readFileSync(rateLimitPath, "utf8");
+    if (fileContent) {
+      rateLimitData = JSON.parse(fileContent);
+    }
+  }
+
+  rateLimitData[requester] = timestamp;
+
+  fs.writeFileSync(rateLimitPath, JSON.stringify(rateLimitData, null, 2));
+}
+
+function checkRateLimit(req) {
+  const requester = req.headers["user-agent"];
+  const rateLimitPath = path.join(__dirname, "rateLimit.json");
+  let rateLimitData = {};
+  if (fs.existsSync(rateLimitPath)) {
+    const fileContent = fs.readFileSync(rateLimitPath, "utf8");
+    if (fileContent) {
+      rateLimitData = JSON.parse(fileContent);
+    }
+  }
+
+  if (rateLimitData[requester]) {
+    const lastRequestTime = new Date(Date.parse(rateLimitData[requester]));
+    const currentTime = new Date();
+    const diff = currentTime - lastRequestTime;
+    if (diff < 30 * 1000) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// exports.app = functions.https.onRequest(app);
